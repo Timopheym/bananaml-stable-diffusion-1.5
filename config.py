a31a6d039b6cf0a1cbcd1c95ee26a74d4c9da34e
Timofey Molchanov <timopheym@gmail.com>
2023-02-11 23:17:45 +0100

    replace done and test

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 model_url = "http://164.90.217.117/model_93612.tar"
 filename = model_url.split('/')[-1]
+model_path = filename.split('.')[0]
+repo = 'jcplus/stable-diffusion-v1-5'
+HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
+base_path = './base_model'
--- download.py
+++ download.py
@@ -1,9 +1,11 @@
 import os
 import torch
 import urllib.request
-from config import model_url, filename
-from diffusers import StableDiffusionPipeline, DDIMScheduler
+from config import model_url, filename, model_path, HF_AUTH_TOKEN, base_path
+from diffusers import StableDiffusionPipeline, DDIMScheduler, DiffusionPipeline, DPMSolverMultistepScheduler
+
 import tarfile
+
 
 
 def untar(filename):
@@ -12,17 +14,34 @@ def untar(filename):
     tar.close()
 
 
+def copy_files(src, dst):
+    folders = os.listdir(src)
+    for folder in folders:
+        # Check if directory
+        full_src_path = os.path.join(src, folder)
+        if os.path.isdir(full_src_path):
+            full_dst_path = os.path.join(dst, folder)
+
+            files = os.listdir(full_src_path)
+            for file in files:
+                full_src_file_path = os.path.join(full_src_path, file)
+                full_dst_file_path = os.path.join(full_dst_path, file)
+                shutil.copy(full_src_file_path, full_dst_file_path)
+
+
 
 def download_model():
-    HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-
+    # Download model
+    scheduler_for_initial_download = DPMSolverMultistepScheduler.from_pretrained(repo, subfolder="scheduler")
+    model_for_initial_download = DiffusionPipeline.from_pretrained(repo, torch_dtype=torch.float16, revision="fp16",
+                                              scheduler=scheduler_for_initial_download,
+                                              use_auth_token=HF_AUTH_TOKEN, safety_checker=None)
+    # Create model folder
+    model_for_initial_download.save_pretrained(base_path)
+    # Download pretrained model
     urllib.request.urlretrieve(model_url, filename)
     untar(filename)
-    model_path = filename.split('.')[0]
-    scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False,
-                              set_alpha_to_one=False)
-    pipe = StableDiffusionPipeline.from_pretrained(model_path, scheduler=scheduler, safety_checker=None,
-                                                   torch_dtype=torch.float16).to("cuda")
-    
+
+    copy_files(model_path, base_path)
 if __name__ == "__main__":
     download_model()
